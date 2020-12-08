b4ce17151324ccb3f955be1d96824741509f99f0
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-12-08 21:14:43 +0100

    started on part two, but failed 😢

--- day06/main.go
+++ day06/main.go
@@ -13,7 +13,10 @@ func main() {
 		panic(err)
 	}
 
-	partOne(input)
+	_ = input
+
+	// partOne(input)
+	partTwo()
 }
 
 func getInput() ([]string, error) {
@@ -49,6 +52,34 @@ func getInput() ([]string, error) {
 	return result, nil
 }
 
+func getInputPartTwo() ([][]string, error) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	var result [][]string
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var group []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if (line == "") {
+			result = append(result, group)
+
+			group = make([]string, 0)
+		} else {
+			group = append(group, line)
+		}
+	}
+
+	return result, nil
+}
+
 func unique(line string) []string {
 	slice := strings.Split(line, "")
 
@@ -75,5 +106,40 @@ func partOne(input []string) {
 		count += uniqueCount
 	}
 
+	fmt.Println("What is the sum of those counts?", count)
+}
+
+func partTwo() {
+	input, err := getInputPartTwo()
+	if err != nil {
+		panic(err)
+	}
+
+	count := 0
+
+	// Go through each group
+	for _, group := range input {
+		// only one in the group
+		if len(group) == 1 {
+			count += len(group[0])
+		} else {
+			// combine all answers in the group
+			joinedAnswers := strings.Join(group, "")
+			uniqueAnswers := unique(joinedAnswers)
+
+			// go through all unqiue answers
+			for _, answer := range uniqueAnswers {
+				// count amount of answers in joined answers
+				answerCount := strings.Count(joinedAnswers, answer)
+	
+				// Check if it occures more then 1 time
+				if answerCount > 1 {
+					count++
+				}
+			}
+		}
+	}
+
+	// 3832 (too high)
 	fmt.Println("What is the sum of those counts?", count)
 }
