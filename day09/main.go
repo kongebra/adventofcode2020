cb764078939b3df963140c7842f5b730b88fc401
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-12-10 09:11:13 +0100

    part two done

--- day09/main.go
+++ day09/main.go
@@ -45,7 +45,7 @@ func init() {
 }
 
 func main() {
-	partOne()
+	// partOne()
 	partTwo()
 }
 
@@ -89,5 +89,83 @@ func partOne() {
 }
 
 func partTwo() {
+	invalid := -1
 
+	for _, value := range input[25:] {
+		valid := false
+
+		for _, sub := range preamble {
+			rest := value - sub
+
+			if rest < 0 {
+				continue
+			}
+
+			for _, check := range preamble {
+				if check == sub {
+					continue
+				}
+
+				if rest == check {
+					preamble = append(preamble, value)
+					valid = true
+					break
+				}
+			}
+
+			if valid {
+				break
+			}
+		}
+
+		if !valid {
+			invalid = value
+			break
+		}
+	}
+	
+	fmt.Println("Invalid:", invalid)
+	invalidSet := make([]int, 0)
+
+	for i := 0; i < len(preamble); i++ {
+		x0 := preamble[i]
+
+		rest := invalid - x0
+
+		for j := i + 1; j < len(preamble); j++ {
+			x1 := preamble[j]
+			
+			if x1 == rest {
+				invalidSet = preamble[i:j+1]
+				break
+			}
+
+			rest -= x1
+
+			if rest < 0 {
+				break
+			}
+		}
+
+		if len(invalidSet) > 0 {
+			break
+		}
+	}
+
+	min := invalidSet[0]
+	max := invalidSet[1]
+	
+	for _, value := range invalidSet {
+		if value > max {
+			max = value
+		}
+
+		if value < min {
+			min = value
+		}
+	}
+	
+	sum := min + max
+
+	fmt.Println("What is the encryption weakness in your XMAS-encrypted list of numbers?", sum)
 }
