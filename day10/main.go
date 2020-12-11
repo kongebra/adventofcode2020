d4facffd560c41341fed195b391978b6fd46d85f
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-12-11 09:30:46 +0100

    started on part two, failed 😢

--- day10/main.go
+++ day10/main.go
@@ -24,7 +24,8 @@ func init() {
 }
 
 func main() {
-	partOne()
+	// partOne()
+	partTwo()
 }
 
 func partOne() {
@@ -82,4 +83,74 @@ func partOne() {
 	}
 
 	fmt.Println("What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?", oneJolts * threeJolts)
+}
+
+type node struct {
+	Value int
+	Children []*node
+}
+
+func newNode(value int) *node {
+	return &node{
+		Value: value,
+		Children: make([]*node, 0),
+	}
+}
+
+func partTwo() {
+	set := make(map[int]*node, 0)
+
+	for _, value := range input {
+		// not in set
+		if _, ok := set[value]; !ok {
+			set[value] = newNode(value)
+		}
+	}
+	
+	// make all Children
+	for k, v := range set {
+		// get node if exists
+		for i := 1; i <= 3; i++ {
+			if n, ok := set[k+i]; ok {
+				v.Children = append(v.Children, n)
+			}
+		}
+	}
+
+	min := utils.FindMin(input)
+	root, ok := set[min]
+	if !ok {
+		panic("panic at the disco")
+	}
+
+	// count := 0
+	// for _, child := range root.Children {
+	// 	if (len(child.Children) == 0) {
+	// 		count++
+	// 	}
+	// }
+
+	paths := 1
+	search(root, func(n int) {
+		fmt.Println(n)
+	})
+	
+	fmt.Println("What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?", paths)
+}
+
+var (
+	visited map[int]bool = make(map[int]bool)
+)
+
+func search(n *node, cb func(int)) {
+	visited[n.Value] = true
+	cb(n.Value)
+
+	for _, v := range n.Children {
+		if visited[v.Value] {
+			continue
+		}
+
+		search(v, cb)
+	}
 }
